e31cd2aa65f24b61d66be335739e732a9cd59c00
Andrey Hryhoriev <andreygrigoryev@ukr.net>
2021-01-05 21:47:56 +0200

    Updates

---
+++ precache-manifest.b188e14e9ca90e1cc138c9f32a45e62d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2dbb4257ffb373b003f7681c228bd71",
+    "url": "/goit-react-hw-04-hooks-images/index.html"
+  },
+  {
+    "revision": "23c25012f714a892df18",
+    "url": "/goit-react-hw-04-hooks-images/static/css/2.f87e9f44.chunk.css"
+  },
+  {
+    "revision": "af253e0058ac6da556e0",
+    "url": "/goit-react-hw-04-hooks-images/static/css/main.de21201d.chunk.css"
+  },
+  {
+    "revision": "23c25012f714a892df18",
+    "url": "/goit-react-hw-04-hooks-images/static/js/2.1d359d8e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-04-hooks-images/static/js/2.1d359d8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af253e0058ac6da556e0",
+    "url": "/goit-react-hw-04-hooks-images/static/js/main.cf59a1df.chunk.js"
+  },
+  {
+    "revision": "cc4c0cfdf041d2830f24",
+    "url": "/goit-react-hw-04-hooks-images/static/js/runtime-main.bd5e2b7d.js"
+  }
+]);
